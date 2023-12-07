301889cf99c8d555c95c37ddbdf97c0dfd5df628
Pepe <j0frus@j0frus.es>
2023-12-07 12:31:29 +0100

    first commit

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,6 +3,23 @@
  * see commented examples below
  */
 
-+(function () {
-  // Swiper
-})();
+const swiper = new Swiper('.mySwiper', {
+  speed: 400,
+  spaceBetween: 500,
+  loop: true,
+  //autoHeight: true,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
